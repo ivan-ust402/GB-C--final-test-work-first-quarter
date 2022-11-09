e182947328dda08231af96c82d1eb46f79f5297b
Ivan Ustyantsev <vangold915@gmail.com>
2022-11-09 19:12:02 +0700

    Added functionality for finding size of resultArray

--- Program.cs
+++ Program.cs
@@ -43,4 +43,15 @@ void PrintArray(string[] array) {
 string[] GetResultArray(string[] array) {
     int count = 0;
     int index = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    
+    string[] resultArray = new string[count];
+
 }
